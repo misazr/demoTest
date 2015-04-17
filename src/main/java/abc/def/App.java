0846ff69d76f06cabd42e76c5a64713519c7031f
misazr <miodrag.seslija@consulteer.com>
2015-04-17 16:04:46 +0200

    3th commit

--- src/main/java/abc/def/App.java
+++ src/main/java/abc/def/App.java
@@ -7,7 +7,7 @@ public class App
 {
 public static void main( String[] args )
 {
-System.out.println( "Hello World! 123145 231 45123 " );
+System.out.println( "Hello World! 123145 231 45123 123" );
 }
 
 }
